c0241487c99c5e217f85719d1d1744048cc38a33
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-02-16 15:07:33 +0700

    add feature B

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Ini adalah branch development");
+        System.out.println("Menambahkan fitur B");
     }
 }
